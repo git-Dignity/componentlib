7dbf4a4f071256367ed4d41ec4465eab0f67b1e7
git-Dignity <1638153584@qq.com>
2022-03-22 10:54:30 +0800

    doc：zzm-pdf-view-down：PDF可查看下载PDF/DOC组件

--- examples/components/PdfViewDownUse.vue
+++ examples/components/PdfViewDownUse.vue
@@ -1,6 +1,18 @@
+<!--
+ * @Author: zemin zheng
+ * @Date: 2022-03-21 11:25:45
+ * @LastEditTime: 2022-03-22 10:49:00
+ * @LastEditors: zemin zheng
+ * @Description: PDF可查看下载PDF/DOC组件 -- 使用例子
+ * 
+ * @Description: 这里提供两个例子
+ * @Description: 一个传参的data，pdf、doc链接都是后端返回走nginx，因为有传docxDown，所以下载是doc的
+ * @Description: 什么都没有传；因为没传有默认值，预览报告test.pdf，又因为没传docxDown，所以下载也是pdf文件的
+ * @FilePath: \componentlib\examples\components\PdfViewDownUse.vue
+-->
 <template>
   <div class="PdfViewDownUse">
-    <zzm-pdf-view-down :data="data" :docxDown="docxDown" @close="close" />
+    <zzm-pdf-view-down :data="data" :docx-down.sync="docxDown" @close="close" />
     <zzm-pdf-view-down @close="close" />
   </div>
 </template>
@@ -10,18 +22,17 @@ export default {
   data() {
     return {
       data: {
-        pdfId: "",
+        pdfUrl: "",
         wordUrl: "",
         pdfTitle: "",
       },
-
       docxDown: true,
     }
   },
   methods: {
     getData() {
       //   rainwater  这个是nginx转发过的
-      this.data.pdfId = encodeURIComponent(
+      this.data.pdfUrl = encodeURIComponent(
         "/rainwater/雨水情小时报-2022032111.pdf"
       )
       this.data.wordUrl = encodeURIComponent(
@@ -34,9 +45,9 @@ export default {
     },
     close(data) {
       console.log(data)
-      this.data.pdfId = ""
-      this.data.wordUrl = ""
-      this.docxDown = false
+      // this.data.pdfUrl = ""
+      // this.data.wordUrl = ""
+      // this.docxDown = false
     },
   },
 
--- packages/pdf-view-down/src/index.vue
+++ packages/pdf-view-down/src/index.vue
@@ -1,10 +1,22 @@
-// 看到水情，还没有看完
+<!--
+ * @Author: zemin zheng
+ * @Date: 2022-03-21 11:23:21
+ * @LastEditTime: 2022-03-22 10:50:43
+ * @LastEditors: zemin zheng
+ * @Description: PDF可查看下载PDF/DOC组件
+ * 
+ * @Description: 可以预览是PDF，但下载我想是word，支持吗？
+ * @Description: sure，只要将docxDown值设为true，传入wordUrl链接即可；若为false，下载的就是pdf
+
+ * @Description: 追问：好奇你是怎么实现的？我记得PDF插件只能下载PDF
+ * @Description：是的，原本只支持下载PDF；但我阅读源码，多写了一个new file：viewerDocxDown.js，就可以支持下载word
+ * @FilePath: \componentlib\packages\pdf-view-down\src\index.vue
+-->
 
 <template>
-  <!-- pdf -->
   <div
     class="wf-popup pdf-container"
-    v-if="data.pdfId"
+    v-if="data.pdfUrl"
     ref="pdf"
     v-directives="{ trigger: '.c-psf-top' }"
   >
@@ -20,11 +32,11 @@
               ? 'pdf/web/viewerDocxDown.html?wordUrl=' +
                 data.wordUrl +
                 '&file=' +
-                data.pdfId
+                data.pdfUrl
               : 'pdf/web/viewer.html?wordUrl=' +
                 data.wordUrl +
                 '&file=' +
-                data.pdfId
+                data.pdfUrl
           "
           frameborder="0"
           width="100%"
@@ -38,24 +50,36 @@
 export default {
   name: "zzm-pdf-view-down",
   props: {
+    // 所需数据
     data: {
       type: Object,
       default: () => {
         return {
           // 报告test：文件是在public\pdf\web文件夹中（预览本地的）
-          pdfId: encodeURIComponent("报告test.pdf"),
-          wordUrl: encodeURIComponent("报告test.docx"),
+          pdfUrl: encodeURIComponent("报告test.pdf"),
+          wordUrl: encodeURIComponent("报告test.docx"), // 好像只支持docx，上次弄个doc不可以
           pdfTitle: "报告test",
         }
       },
     },
+    // 是否下载的是word
     docxDown: {
       type: Boolean,
       default: false,
     },
   },
   methods: {
+    /**
+     * @description: 关闭事件
+     * @description: 不可以直接修改docxDown的值，通过父组件设置sync，子组件使用update改变其值
+     * @param {*}
+     * @return {*}
+     */
     close() {
+      this.data.pdfUrl = ""
+      this.data.wordUrl = ""
+      // this.docxDown = false
+      this.$emit("update:docxDown", false)
       this.$emit("close", true)
     },
   },
@@ -63,20 +87,20 @@ export default {
 </script>
 <style lang="less" scoped>
 .wf-popup {
-    position: relative;
-    width: 100%;
-    height: 100%;
-//   position: absolute;
-//   top: 0;
-//   left: 0;
-//   right: 0;
-//   bottom: 0;
+  position: relative;
+  width: 100%;
+  height: 100%;
+  //   position: absolute;
+  //   top: 0;
+  //   left: 0;
+  //   right: 0;
+  //   bottom: 0;
   display: flex;
   flex-direction: column;
   background-color: #fff; // #f7f8fa
   z-index: 2;
   font-size: 16px;
-  
+
   .wf-close {
     position: absolute;
     top: 15px;
